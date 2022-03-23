b46efea6730afc66cd3ede323e0db1bee0475062
泠然 <zhangchaozhe@cai-inc.com>
2022-03-23 16:20:09 +0800

    chmod 777

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,33 +1,34 @@
 /**
  * electron 主文件
  */
-import '@src/common/patch'
-import { join } from 'path'
-import { app, BrowserWindow } from 'electron'
-import dotenv from 'dotenv'
+import "@src/common/patch";
+import { join } from "path";
+import os from "os";
+import { app, BrowserWindow } from "electron";
+import dotenv from "dotenv";
 
-dotenv.config({ path: join(__dirname, '../../.env') })
+dotenv.config({ path: join(__dirname, "../../.env") });
 
-let win: BrowserWindow
+let win: BrowserWindow;
 
 function createWin() {
-	// 创建浏览器窗口
-	win = new BrowserWindow({
-		width: 1024,
-		height: 768,
-		frame: false,
-		webPreferences: {
-			nodeIntegration: true,
-			contextIsolation: false,
-			preload: join(__dirname, '../../src/preload/index.js'),
-		},
-	})
+  // 创建浏览器窗口
+  win = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    frame: os.platform() === "darwin" ? true : false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      preload: join(__dirname, "../../src/preload/index.js"),
+    },
+  });
 
-	const URL = app.isPackaged
-		? `file://${join(__dirname, '../render/index.html')}` // vite 构建后的静态文件地址
-		: `http://localhost:${process.env.PORT}` // vite 启动的服务器地址
+  const URL = app.isPackaged
+    ? `file://${join(__dirname, "../render/index.html")}` // vite 构建后的静态文件地址
+    : `http://localhost:${process.env.PORT}`; // vite 启动的服务器地址
 
-	win?.loadURL(URL)
+  win?.loadURL(URL);
 }
 
-app.whenReady().then(createWin)
+app.whenReady().then(createWin);
